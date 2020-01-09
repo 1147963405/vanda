3a7c1ccc0a1fbd50508e29dc9377c97f75438810
liyuwu <liyuwu@example.com>
2020-01-09 17:21:28 +0800

    提交1

---
+++ eureka/src/main/java/com/example/eurekademo/Application.java
@@ -0,0 +1,17 @@
+package com.example.eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//注册中心
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		//System.out.println("hello world");
+	}
+
+}
